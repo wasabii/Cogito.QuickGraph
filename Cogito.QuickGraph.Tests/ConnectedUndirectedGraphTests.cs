e57c40e7e40cfaeb0dba02cf81d12819aa62f27d
Jerome Haltom <wasabi@cogito.cx>
2018-11-03 11:16:38 -0500

    Added a graph that fully connects all vertices.

---
+++ Cogito.QuickGraph.Tests/ConnectedUndirectedGraphTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Cogito.QuickGraph.Tests
+{
+
+    [TestClass]
+    public class ConnectedUndirectedGraphTests
+    {
+
+        [TestMethod]
+        public void Can_create()
+        {
+            var v = new[] { "v1", "v2", "v3", "v4" };
+            var g = new ConnectedUndirectedGraph<string>(v);
+
+            g.AdjacentEdges("v1").Select(i => i.Target).Should().HaveCount(3).And.Contain(new[] { "v2", "v3", "v4" });
+            g.AdjacentEdges("v2").Select(i => i.Target).Should().HaveCount(3).And.Contain(new[] { "v1", "v3", "v4" });
+            g.AdjacentEdges("v3").Select(i => i.Target).Should().HaveCount(3).And.Contain(new[] { "v1", "v2", "v4" });
+            g.AdjacentEdges("v4").Select(i => i.Target).Should().HaveCount(3).And.Contain(new[] { "v1", "v2", "v3" });
+        }
+
+
+    }
+
+}
---
+++ Cogito.QuickGraph/ConnectedUndirectedGraph.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using QuickGraph;
+
+namespace Cogito.QuickGraph
+{
+
+    /// <summary>
+    /// An undirected graph which wraps another undirected graph
+    /// </summary>
+    /// <typeparam name="TVertex"></typeparam>
+    public class ConnectedUndirectedGraph<TVertex> :
+        DelegateUndirectedGraph<TVertex, SEquatableUndirectedEdge<TVertex>>
+    {
+
+        /// <summary>
+        /// Returns the edges for a given dimension value.
+        /// </summary>
+        /// <param name="getter"></param>
+        /// <returns></returns>
+        static global::QuickGraph.TryFunc<T, TResult> GetTryGetFunc<T, TResult>(Func<T, TResult> getter)
+        {
+            // capture implementation
+            bool TryGetFuncImpl(T arg, out TResult result)
+            {
+                result = getter(arg);
+                return result != null;
+            }
+
+            // return internal impl
+            return TryGetFuncImpl;
+        }
+
+        /// <summary>
+        /// Gets the adjacent edges for the specified vertex.
+        /// </summary>
+        /// <param name="vertices"></param>
+        /// <param name="vertext"></param>
+        /// <returns></returns>
+        static IEnumerable<SEquatableUndirectedEdge<TVertex>> GetAdjacentEdges(IEnumerable<TVertex> vertices, TVertex vertex)
+        {
+            if (vertices == null)
+                throw new ArgumentNullException(nameof(vertices));
+
+            return vertices
+                .Where(i => !Equals(i, vertex))
+                .Select(i => new SEquatableUndirectedEdge<TVertex>(vertex, i));
+        }
+
+        public ConnectedUndirectedGraph(IEnumerable<TVertex> vertices) : 
+            base(vertices, GetTryGetFunc<TVertex, IEnumerable<SEquatableUndirectedEdge<TVertex>>>(v => GetAdjacentEdges(vertices, v)), false)
+        {
+
+        }
+
+    }
+
+}
